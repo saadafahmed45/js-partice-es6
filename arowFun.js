5a263f7687c485878c2983296fbf303aba73f291
saadafahmed45 <hafijnahid45@.com>
2024-01-02 19:03:24 +0600

    first commit

---
+++ arowFun.js
@@ -0,0 +1,15 @@
+// this is normal function
+function name() {
+  return "saadaf,ahmed";
+}
+console.log(name());
+
+// this is arowfuntion
+
+const number = () => {
+  console.log(54);
+};
+number();
+
+
+// Iuser this keyword for arowfuntion used 
